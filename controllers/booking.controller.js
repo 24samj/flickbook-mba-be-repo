dbb18be83ad5339ddecdf40be0e006e7636537a6
Mrigank Kaushik <mriganknitw@gmail.com>
2023-09-18 22:25:44 +0530

    Addressed security concern in Get all booking API

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,20 +1,35 @@
 const { USERTYPES } = require("../constant");
 const Booking = require("../models/booking.model");
+const Theatre = require("../models/theatre.model");
 const User = require("../models/user.model");
 
 async function getAllBookings(req, res) {
-  // if it is an admin or owner, return all bookings
+  // if it is an admin, return all bookings
+  // if it is an owner, then return bookings for their theatres only
   // if it is a customer, then return only their booking
 
-  if (req.userType === USERTYPES.CUSTOMER) {
-    const userId = req.userId;
-    const user = await User.find({ userId: userId });
-    const queryObj = {};
-    queryObj.userId = user._id;
+  if (req.userType === USERTYPES.ADMIN) {
+    const bookings = await Booking.find();
+    res.status(200).send(bookings);
+    return;
   }
 
-  const bookings = await Booking.find(queryObj);
-  res.status(200).send(bookings);
+  const userId = req.userId;
+  const user = await User.find({ userId: userId });
+
+  if (req.userType === USERTYPES.CUSTOMER) {
+    const bookings = await Booking.find({ userId: user._id });
+    res.status(200).send(bookings);
+  } else {
+    // user is a owner
+    const theatresOwned = await Theatre.find({ ownerId: user._id }).select(
+      "_id"
+    );
+    const bookingsInOwnedTheatres = await Booking.find({
+      theatreId: { $in: theatresOwned },
+    });
+    res.status(200).send(bookingsInOwnedTheatres);
+  }
 }
 
 async function getBookingById(req, res) {
