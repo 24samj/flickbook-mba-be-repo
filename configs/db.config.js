0f5036ec4d07270ba49a717fa358a12268228871
Sumit Chakraborty <csumit961@gmail.com>
2023-11-09 21:18:23 +0530

    mba-be-code-2

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    DB_NAME: "mba_db2",
+    DB_NAME: "mba_db",
     DB_URL:
         process.env.MONGODB_URI ||
-        `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.mob0anj.mongodb.net/mba_db2`,
+        `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.mob0anj.mongodb.net/mba_db`,
 };
