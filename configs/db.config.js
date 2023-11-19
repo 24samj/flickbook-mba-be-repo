96aa2db64e3d9c87d2098995faa685ac1f4bc976
Sumit Chakraborty <csumit961@gmail.com>
2023-11-19 10:28:31 +0530

    final-build

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    DB_URL: process.env.DB_URL,
+    DB_NAME: "mba_db",
+    DB_URL:
+        process.env.MONGODB_URI ||
+        `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.mob0anj.mongodb.net/mba_db
+        `,
 };
