365196dd1d2272bec4354aa20be4993f3cbb0987
Sumit Chakraborty <csumit961@gmail.com>
2023-11-20 10:28:12 +0530

    final-build

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,60 +4,82 @@ const bcrypt = require("bcrypt");
 const sendMail = require("../utils/notification");
 
 async function getAllUsers(req, res) {
-    const users = await User.find().select("-password");
-    res.send(users);
+    try {
+        const users = await User.find().select("-password");
+        res.send(users);
+    } catch (error) {
+        console.error("Error in getAllUsers:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
 }
 
 async function updateUserStatus(req, res) {
-    const { body } = req;
-    const { id } = req.params;
+    try {
+        const { body } = req;
+        const { id } = req.params;
 
-    const updatedUser = await User.findByIdAndUpdate(
-        mongoose.Types.ObjectId(id),
-        { userStatus: body.userStatus },
-        { new: true } // This option returns the modified document rather than the original one
-    );
+        const updatedUser = await User.findByIdAndUpdate(
+            mongoose.Types.ObjectId(id),
+            { userStatus: body.userStatus },
+            { new: true } // This option returns the modified document rather than the original one
+        );
 
-    sendMail(
-        id,
-        "User status has changed",
-        `Your userStatus has been changed to ${req.body.userStatus}.`,
-        [updatedUser.email]
-    );
+        if (!updatedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
 
-    res.status(200).send(updatedUser);
+        await sendMail(
+            id,
+            "User status has changed",
+            `Your userStatus has been changed to ${req.body.userStatus}.`,
+            [updatedUser.email]
+        );
+
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        console.error("Error in updateUserStatus:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
 }
 
 async function updateUserDetails(req, res) {
-    const { body } = req;
-    const { id } = req.params;
+    try {
+        const { body } = req;
+        const { id } = req.params;
 
-    const user = await User.findOne({ userId: req.userId });
+        const user = await User.findOne({ userId: req.userId });
 
-    if (user._id.toString() !== id) {
-        res.status(403).send({
-            message: "Cannot update the details of a user other than self",
-        });
-        return;
+        if (!user || user._id.toString() !== id) {
+            return res.status(403).send({
+                message: "Cannot update the details of a user other than self",
+            });
+        }
+
+        const updateObj = {};
+
+        updateObj.userId = body.userId;
+        updateObj.email = body.email;
+        updateObj.name = body.name;
+
+        if (body.password) {
+            updateObj.password = bcrypt.hashSync(body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            mongoose.Types.ObjectId(id),
+            updateObj,
+            { new: true } // This option returns the modified document rather than the original one
+        );
+
+        if (!updatedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        console.error("Error in updateUserDetails:", error);
+        res.status(500).send({ message: "Internal Server Error" });
     }
-
-    const updateObj = {};
-
-    updateObj.userId = body.userId;
-    updateObj.email = body.email;
-    updateObj.name = body.name;
-
-    if (body.password) {
-        updateObj.password = bcrypt.hashSync(body.password, 10);
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(
-        mongoose.Types.ObjectId(id),
-        updateObj,
-        { new: true } // This option returns the modified document rather than the original one
-    );
-
-    res.status(200).send(updatedUser);
 }
 
 module.exports = {
