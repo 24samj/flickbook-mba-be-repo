6e0dbea819beeef92d8ca5649f4d528b8d5dd4d3
Sumit Chakraborty <csumit961@gmail.com>
2023-11-22 19:06:30 +0530

    final-build

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,14 +1,24 @@
-const {
-  createPayment,
-  getAllPayments,
-  getPaymentById,
-} = require("../controllers/payment.controller");
-const { verifyToken, isAdmin } = require("../middlewares/authJwt");
+const paymentController = require("../controllers/payment.controller");
+const { authJwt, verifyPaymentReqBody } = require("../middlewares");
+
+/**
+ * Routes for the booking resource
+ */
 
 module.exports = function (app) {
-  app.post("/mba/api/v1/payments", [verifyToken], createPayment);
-
-  app.get("/mba/api/v1/payments", [verifyToken], getAllPayments);
-
-  app.get("/mba/api/v1/payments/:id", [verifyToken], getPaymentById);
+    app.get(
+        "/mba/api/v1/payments",
+        [authJwt.verifyToken],
+        paymentController.getAllPayments
+    );
+    app.get(
+        "/mba/api/v1/payments/:id",
+        [authJwt.verifyToken],
+        paymentController.getPaymentOnId
+    );
+    app.post(
+        "/mba/api/v1/payments",
+        [authJwt.verifyToken, verifyPaymentReqBody.validatePaymentRequestBody],
+        paymentController.createPayment
+    );
 };
