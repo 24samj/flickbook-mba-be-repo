4e00de768d65f9becdc04ab314c7260cf580b672
Sumit Chakraborty <csumit961@gmail.com>
2023-11-28 13:10:17 +0530

    final-build

--- routes/theatre.route.js
+++ routes/theatre.route.js
@@ -1,40 +1,44 @@
 const {
-  getAllTheatres,
-  getTheatreById,
-  createTheatre,
-  updateTheatre,
-  deleteTheatre,
-  addMoviesToATheatre,
+    getAllTheatres,
+    getTheatreById,
+    createTheatre,
+    updateTheatre,
+    deleteTheatre,
+    addMoviesToATheatre,
 } = require("../controllers/theatre.controller");
 const {
-  verifyToken,
-  isAdminOrClient,
-  isAdmin,
+    verifyToken,
+    isAdminOrClient,
+    isAdmin,
 } = require("../middlewares/authJwt");
 const isTheatreOwnerOrAdmin = require("../middlewares/theatre");
 
 module.exports = function (app) {
-  app.get("/mba/api/v1/theatres", [verifyToken], getAllTheatres);
+    app.get("/mba/api/v1/theatres", [verifyToken], getAllTheatres);
 
-  app.get("/mba/api/v1/theatres/:id", [verifyToken], getTheatreById);
+    app.get("/mba/api/v1/theatres/:id", [verifyToken], getTheatreById);
 
-  app.post("/mba/api/v1/theatres", [verifyToken, isAdmin], createTheatre);
+    app.post(
+        "/mba/api/v1/theatres",
+        [verifyToken, isAdminOrClient],
+        createTheatre
+    );
 
-  app.put(
-    "/mba/api/v1/theatres/:id",
-    [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
-    updateTheatre
-  );
+    app.put(
+        "/mba/api/v1/theatres/:id",
+        [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
+        updateTheatre
+    );
 
-  app.delete(
-    "/mba/api/v1/theatres/:id",
-    [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
-    deleteTheatre
-  );
+    app.delete(
+        "/mba/api/v1/theatres/:id",
+        [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
+        deleteTheatre
+    );
 
-  app.put(
-    "/mba/api/v1/theatres/:id/movies",
-    [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
-    addMoviesToATheatre
-  );
+    app.put(
+        "/mba/api/v1/theatres/:id/movies",
+        [verifyToken, isAdminOrClient, isTheatreOwnerOrAdmin],
+        addMoviesToATheatre
+    );
 };
