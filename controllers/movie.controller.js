fac831f3a2f13c807f89b6d4d76ff99739342ec6
Sumit Chakraborty <csumit961@gmail.com>
2023-11-28 14:59:41 +0530

    final-build

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,51 +1,51 @@
 const Movie = require("../models/movie.model");
 
 async function getAllMovies(req, res) {
-  const movies = await Movie.find();
-  res.status(200).send(movies);
+    const movies = await Movie.find();
+    res.status(200).send(movies);
 }
 
 async function getMovieById(req, res) {
-  try {
-    const id = req.params.id;
-    const movie = await Movie.findById(id);
+    try {
+        const id = req.params.id;
+        const movie = await Movie.findById(id);
 
-    res.status(200).send(movie);
-  } catch (ex) {
-    res.status(404).send({
-      message: "Movie id does not exist",
-    });
-  }
+        res.status(200).send(movie);
+    } catch (ex) {
+        res.status(404).send({
+            message: "Movie id does not exist",
+        });
+    }
 }
 
 async function createMovie(req, res) {
-  const movie = req.body;
+    const movie = req.body;
 
-  const createdMovie = await Movie.create(movie);
+    const createdMovie = await Movie.create(movie);
 
-  res.status(201).send(createdMovie);
+    res.status(201).send(createdMovie);
 }
 
 async function updateMovie(req, res) {
-  const id = req.params.id;
+    const id = req.params.id;
+    console.log(req.body);
+    const updatedMovie = await Movie.findByIdAndUpdate(id, req.body);
 
-  const updatedMovie = await Movie.findByIdAndUpdate(id, req.body);
-
-  res.send(updatedMovie);
+    res.send(updatedMovie);
 }
 
 async function deleteMovie(req, res) {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  await Movie.findByIdAndDelete(id);
+    await Movie.findByIdAndDelete(id);
 
-  res.send();
+    res.send();
 }
 
 module.exports = {
-  getAllMovies,
-  getMovieById,
-  createMovie,
-  updateMovie,
-  deleteMovie,
+    getAllMovies,
+    getMovieById,
+    createMovie,
+    updateMovie,
+    deleteMovie,
 };
